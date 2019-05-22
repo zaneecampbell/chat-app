70f2005ca0cace1b16b9b32ebe8045ed966d4d86
Zane Campbell <zanegitinit@gmail.com>
2019-05-22 11:36:00 -0700

    added timestamps for share location messages

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const http = require('http')
 const express = require('express')
 const socketio = require('socket.io')
 const Filter = require('bad-words')
-const { generateMessage } = require('./utils/messages')
+const { generateMessage, generateLocationMessage } = require('./utils/messages')
 
 const app = express()
 // creating the server outside the express library so we can configure it for socket.io otherwise express does this for us
@@ -34,7 +34,7 @@ io.on('connection', (socket) => {
     })
 
     socket.on('sendLocation', (coords, callback) => {
-        io.emit('locationMessage', `https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
+        io.emit('locationMessage', generateLocationMessage(`https://google.com/maps?q=${coords.latitude},${coords.longitude}`))
         callback()
     })
 
